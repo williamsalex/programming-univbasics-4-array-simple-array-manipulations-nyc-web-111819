72f56c58d24862d7d58d01355e94da2ff4008c79
Alexander Williams <williams_alex@protonmail.com>
2019-10-04 18:51:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,11 +10,39 @@ def using_pop(array)
   array.pop
 end
 
-def pop_with_args(array, num)
-  array.pop(num)
+def pop_with_args(array)
+  array.pop(2)
 end
 
 def using_shift(array)
   array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array1, array2)
+  array1+array2
+end
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(arrays)
+  arrays.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, int)
+  array.delete(int)
 end
 

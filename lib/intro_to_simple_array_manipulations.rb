3f5a10faa1b1971ef456021ccdf00df2cca1b46b
Alexander Williams <williams_alex@protonmail.com>
2019-10-04 18:47:49 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,20 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array,string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array, num)
+  array.pop(num)
+end
+
+def using_shift(array)
+  array.shift
+end
+
